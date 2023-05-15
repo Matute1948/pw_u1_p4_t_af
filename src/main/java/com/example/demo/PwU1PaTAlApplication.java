44b7c79f581b7a43953c0cd396c3e6d8ee82ad32
Matute1948 <achigfelipe@gmail.com>
2023-05-15 13:18:13 -0500

    Cuadrado con asteriscos

--- src/main/java/com/example/demo/PwU1PaTAlApplication.java
+++ src/main/java/com/example/demo/PwU1PaTAlApplication.java
@@ -15,7 +15,15 @@ public class PwU1PaTAlApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo tarea3");
-		
+		//Como cambio un rombo
+		for (int i = 0; i < 10; i++) {
+			for(int j = 0; j < 10; j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+			
+			
+		}
 		
 	}
 
