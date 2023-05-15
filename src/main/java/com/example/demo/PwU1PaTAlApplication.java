1b7c821701d42cc804800750b4611fb2ea30942f
Matute1948 <achigfelipe@gmail.com>
2023-05-15 12:36:41 -0500

    first commit

---
+++ src/main/java/com/example/demo/PwU1PaTAlApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwU1PaTAlApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwU1PaTAlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo tarea3");
+		
+		
+	}
+
+}
